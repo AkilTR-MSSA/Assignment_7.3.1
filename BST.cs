b7d190c5f57d08c8a4b1b9b238f5f41df0ce4a7a
Akil Turner-Richards <ki1orich@gmail.com>
2025-05-22 13:41:01 -0700

    Add project files.

---
+++ BST.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+public class Node
+{
+    public int Data;
+    public Node Left;
+    public Node Right;
+
+    public Node(int data, Node left = null, Node right = null)
+    {
+        Data = data;
+        Left = left;
+        Right = right;
+    }
+}
+
+public class BST
+{
+    public Node Root;
+
+    public BST()
+    {
+        Root = null;
+    }
+
+    public void InsertNode(int val)
+    {
+        Node newNode = new Node(val);
+        if (Root == null)
+        {
+            Root = newNode;
+            return;
+        }
+        Node current = Root;
+        Node parent = null;
+        while (current != null)
+        {
+            parent = current;
+            if (val == current.Data)
+            {
+                return;
+            }
+            else if (val < current.Data)
+            {
+                current = current.Left;
+            }
+            else
+            {
+                current = current.Right;
+            }
+        }
+        if (val < parent.Data)
+        {
+            parent.Left = newNode;
+        }
+        else
+        {
+            parent.Right = newNode;
+        }
+    }
+
+    public Node Search(int n)
+    {
+        Node temp = Root;
+        while (temp != null)
+        {
+            if (temp.Data == n)
+            {
+                return temp;
+            }
+            else if (n < temp.Data)
+            {
+                temp = temp.Left;
+            }
+            else
+            {
+                temp = temp.Right;
+            }
+        }
+        return null;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = { 30, 87, 21, 14, 99, 53, 6, 105, 20};
+BST tree = new BST();
+foreach (int i in arr)
+{
+    tree.InsertNode(i);
+}
+Node node = tree.Search(21);
+if  (node != null)
+{
+    Console.WriteLine($"Found value: {node.Data}");
+}
+else
+{
+    Console.WriteLine("Value not found!");
+}
